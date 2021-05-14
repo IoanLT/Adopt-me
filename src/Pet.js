dcfa602cb7bd40ce8fa9a154c479fb35d868c7bc
IoanLT <ioan.tranole@gmail.com>
2021-05-14 16:44:30 +0100

    updating package.json files and adding jsx to our App and Pet components

--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,22 @@
 import React from "react";
 // import ReactDOM from "react-dom";
 
-const Pet = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese"),
-  ]);
-};
+// const Pet = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, "Luna"),
+//     React.createElement("h2", {}, "Dog"),
+//     React.createElement("h2", {}, "Havanese"),
+//   ]);
+// };
+
+const Pet = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h3>{props.breed}</h3>
+    </div>
+  )
+}
 
 export default Pet;
