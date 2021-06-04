253b4787d531d03f54d8a2dbf7a30a60b3278260
IoanLT <ioan.tranole@gmail.com>
2021-06-04 11:31:05 +0100

    created Details and Carousel as class components and fetching our data from the API

--- Details.js
+++ Details.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
+class Details extends Component {
+  state = {
+    loading: true,
+  };
 
-const Details = () => {
-  return (
-    <div>
-      <h2>Hello there!</h2>
-    </div>
-  )
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+  
+
+  render() {    
+    const { animal, breed, city, state, description, name, images } = this.state;
+    
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    }
+
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Details
+export default withRouter(Details);
