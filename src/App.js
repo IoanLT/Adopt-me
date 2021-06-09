57e696774d9528cf19259db6cf5eee4a0f38d1ea
IoanLT <ioan.tranole@gmail.com>
2021-06-09 17:42:45 +0100

    importing useSelector and useDispatch hook from redux in the SearchParams components to be able to use that information from the store and dispatch all actions

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
-const App = () => {
-  const theme = useState("darkblue");
+const App = () => {  
 
   return (
     <Provider store={store}>
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,22 @@
-import { useEffect, useState, useContext } from "react";
-import ThemeContext from "../ThemeContext";
-// import Pet from "./Pet";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Results from "./Results";
 import useBreedList from "./useBreedList";
+import changeLocation from "./actionCreators/changeLocation";
+import changeAnimal from "./actionCreators/changeAnimal";
+import changeBreed from "./actionCreators/changeBreed";
+import changeTheme from "./actionCreators/changeTheme";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [animal, updateAnimal] = useState("");
-  const [location, updateLocation] = useState("");
-  const [breed, updateBreed] = useState("");
+  const animal = useSelector((store) => store.animal);
+  const location = useSelector((store) => store.location);
+  const breed = useSelector((store) => store.breed);
+  const theme = useSelector((store) => store.theme);
   const [pets, setPets] = useState([]);
   const [breeds] = useBreedList(animal);
-  const [theme] = useContext(ThemeContext);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     requestPets();
@@ -25,6 +29,11 @@ const SearchParams = () => {
     const json = await res.json();
 
     setPets(json.pets);
+  }
+
+  function handleAnimalChange(e) {
+    dispatch(changeBreed(''));
+    dispatch(changeAnimal(e.target.value));
   }
 
   return (
@@ -41,7 +50,7 @@ const SearchParams = () => {
             id="location"
             value={location}
             placeholder="Location"
-            onChange={(e) => updateLocation(e.target.value)}
+            onChange={(e) => dispatch(changeLocation(e.target.value))}
           />
         </label>
         <label htmlFor="animal">
@@ -49,8 +58,8 @@ const SearchParams = () => {
           <select
             id="animal"
             value={animal}
-            onChange={(e) => updateAnimal(e.target.value)}
-            onBlur={(e) => updateAnimal(e.target.value)}
+            onChange={handleAnimalChange}
+            onBlur={handleAnimalChange}
           >
             <option />
             {ANIMALS.map((animal) => (
@@ -66,8 +75,8 @@ const SearchParams = () => {
             disabled={!breeds.length}
             id="breed"
             value={breed}
-            onChange={(e) => updateBreed(e.target.value)}
-            onBlur={(e) => updateBreed(e.target.value)}
+            onChange={(e) => dispatch(changeBreed(e.target.value))}
+            onBlur={(e) => dispatch(changeBreed(e.target.value))}
           >
             <option />
             {breeds.map((breed) => (
